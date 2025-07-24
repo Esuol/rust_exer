c9484abc87c96d0a2d4d1bfd246a3959499f5641
berlinen <1292393108@qq.com>
2025-07-24 08:08:23 +0800

    feat: partialOrd

--- exercises/add/adder/src/main.rs
+++ exercises/add/adder/src/main.rs
@@ -197,3 +197,23 @@ fn run_pair() {
     let pair = Pair::new(1, 2);
     pair.cmp_display();
 }
+
+fn run_result_5() {
+    let f = File::open("hello.txt");
+    let f = match f {
+        Ok(file) => file,
+        Err(e) => {
+            panic!("Problem opening the file: {e}");
+        }
+    };
+
+    let f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {error}");
+            })
+        } else {
+            panic!("Problem opening the file: {error}");
+        }
+    });
+}
