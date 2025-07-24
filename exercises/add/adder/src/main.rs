60e2483fee11457743640457e04131b74734720e
berlinen <1292393108@qq.com>
2025-07-24 08:08:37 +0800

    feat: partialOrd

--- exercises/add/adder/src/main.rs
+++ exercises/add/adder/src/main.rs
@@ -217,3 +217,16 @@ fn run_result_5() {
         }
     });
 }
+
+
+fn run_result_6() {
+    let f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {error}");
+            })
+        } else {
+            panic!("Problem opening the file: {error}");
+        }
+    });
+}
