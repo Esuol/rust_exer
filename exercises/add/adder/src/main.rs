31f70c3d3172472d1def6c34c5a1db6efa66af76
berlinen <1292393108@qq.com>
2025-08-01 15:57:08 +0800

    chore: shortest

--- exercises/add/adder/src/main.rs
+++ exercises/add/adder/src/main.rs
@@ -245,6 +245,14 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     }
 }
 
+fn shortest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() < y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn run_longest() {
     let string1 = String::from("abcd");
     let string2 = "xyz";
