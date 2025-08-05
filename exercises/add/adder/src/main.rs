a94e9f1856c05fb28864c217c86a489d62ff9911
berlinen <1292393108@qq.com>
2025-08-05 16:22:16 +0800

    feat: longest_with_an_announcement

--- exercises/add/adder/src/main.rs
+++ exercises/add/adder/src/main.rs
@@ -253,6 +253,18 @@ fn shortest<'a>(x: &'a str, y: &'a str) -> &'a str {
     }
 }
 
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display + Copy,
+{
+    println!("Announcement! {ann}");
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn run_longest() {
     let string1 = String::from("abcd");
     let string2 = "xyz";
