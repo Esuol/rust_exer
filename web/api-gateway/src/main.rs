7bee112401df497a3920895fb219a483e4c4470a
berlinen <1292393108@qq.com>
2025-09-03 15:44:33 +0800

    feat: test routes

--- web/api-gateway/src/main.rs
+++ web/api-gateway/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{get, launch, routes};
+
+#[get("/")]
+fn index() -> &'static str {
+    "Welcome to API Gateway!"
+}
+
+#[get("/health")]
+fn health() -> &'static str {
+    "OK"
+}
+
+#[launch] // entry point 启动宏
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, health]) // 注册路由
 }
